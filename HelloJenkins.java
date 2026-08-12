987ab285f10df611b376f85940d88324d4a71081
Ubuntu <azureuser@jenkins-azure.fg4sxvcjgyeu1hxpplbuzjjhgf.hx.internal.cloudapp.net>
2026-08-12 17:55:03 +0000

    Initial Java Jenkins project

---
+++ HelloJenkins.java
@@ -0,0 +1,9 @@
+public class HelloJenkins {
+    public static void main(String[] args) {
+        System.out.println("=================================");
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Build executed on Azure Agent.");
+        System.out.println("Java project built successfully.");
+        System.out.println("=================================");
+    }
+}
